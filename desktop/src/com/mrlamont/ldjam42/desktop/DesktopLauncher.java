2a422830c5b3b082d25a372216d998e6440ae695
lamon <lamon@DESKTOP-43O746I>
2018-08-11 09:30:11 -0400

    Initial commit

---
+++ desktop/src/com/mrlamont/ldjam42/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mrlamont.ldjam42.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mrlamont.ldjam42.LDJAM42Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LDJAM42Game(), config);
+	}
+}
